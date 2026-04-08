e5885499c3a573012bef6563e305f6a1afd85ae8
Guilherme Bigois <guilhermebigois@outlook.com>
2026-04-08 02:39:47 -0300

    feat: add form field styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,20 +3,43 @@ import './Form.css';
 // 1. Passagem de properiedades via spread operator
 const SpreadInputProps = (props) => {
     return (
-        <input {...props}/>
+        <div className={'field-group'}>
+            <label className={'form-label'} htmlFor={props.id}>{props.title}</label>
+            <input {...props}/>
+        </div>
     );
-}
+};
+
+const SpreadSelectProps = (props) => {
+    return (
+        <div className={'field-group'}>
+            <label className={'form-label'} htmlFor={props.id}>{props.title}</label>
+            <select {...props}>{props.children}</select>
+        </div>
+    );
+};
 
 // 2. Passagem de properiedades via children
 export const Form = (props) => {
     return (
         <form className={'form'}>
-            <h2>{props.children}</h2>
-            <fieldset>
+            <legend className={'form-title'}>{props.children}</legend>
+            <fieldset className={'form-fiedlset'}>
                 {/* 3. Passagem de properiedades via props */}
-                <label htmlFor={'name'}>{props.title}</label>
-                <SpreadInputProps type={'text'} id={'name'} name={'name'} placeholder={'Summer Dev Hits'}/>
+                <div className={'field-group-list'}>
+                    <SpreadInputProps className={'form-input'} type={'text'} id={'name'} name={'name'}
+                                      placeholder={'Summer Dev Hits'} title={'Nome do evento'}/>
+                    <SpreadInputProps className={'form-input'} type={'date'} id={'date'} name={'date'}
+                                      placeholder={'XX/XX/XXXX'} title={'Data do evento'}/>
+                    <SpreadSelectProps id={'theme'} name={'theme'} className={'form-input'} title={'Tema do evento'}>
+                        <option value=''>Selecione uma opção</option>
+                        <option value='tech'>Tecnologia</option>
+                        <option value='music'>Música</option>
+                        <option value='sports'>Esportes</option>
+                        <option value='food'>Gastronomia</option>
+                    </SpreadSelectProps>
+                </div>
             </fieldset>
         </form>
     );
-}
+};
